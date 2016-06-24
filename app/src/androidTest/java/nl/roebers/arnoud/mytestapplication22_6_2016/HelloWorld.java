f648bd00a0d099d221003a3433a5c40510428d30
Arnoud Roebers <Arnoud Roebers>
2016-06-24 22:16:19 +0200

    Initial commit

---
+++ app/src/androidTest/java/nl/roebers/arnoud/mytestapplication22_6_2016/HelloWorld.java
@@ -0,0 +1,14 @@
+package nl.roebers.arnoud.mytestapplication22_6_2016;
+/**
+ * * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+
+/**
+ * @author Arnoud Roebers
+ *
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello world!");}
+}
